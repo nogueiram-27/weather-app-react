262ffdb49723cdb3baed5d3990522a0cf44030ee
nogueiram-27 <64786262+nogueiram-27@users.noreply.github.com>
2020-12-27 14:37:21 +0000

    removed <a> html component
    nogueiram-27 20201227
    
    . removed the link in the unit selection to avoid getting the error of an invalid href

--- src/WeatherUnit.js
+++ src/WeatherUnit.js
@@ -5,14 +5,8 @@ import "./WeatherUnit.css";
 export default function WeatherUnit() {
   return (
     <div className="WeatherUnit">
-      <p className="weather-unit">
-        <a className="convert-unit selected" href="#" id="convert-celsius">
-          ºC
-        </a>
-        |
-        <a className="convert-unit" href="#" id="convert-fahrenheit">
-          ºF
-        </a>
+      <p className=" weather-unit convert-unit selected" id="convert-celsius">
+        ºC | ºF
       </p>
     </div>
   );
